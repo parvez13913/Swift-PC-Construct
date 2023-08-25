fbcc511bb0c3074273d54e817c1b0c703d422d95
parvez13913 <parvezz13913@gmail.com>
2023-08-25 14:50:23 +0600

    [DONE] home page design

---
+++ src/components/Ui/FeaturedProduct.js
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import React from 'react';
+
+const FeaturedProduct = ({ product }) => {
+    const { image, productName, category, price, status, rating } = product;
+    return (
+        <Link href="/" className="card bg-base-100 shadow-xl flex flex-col border border-info my-8">
+            <figure>
+                <img
+                    src={image}
+                    alt="Product"
+                    style={{ aspectRatio: "300 / 200" }}
+                    className=" w-[300px] h-[200px]"
+                />
+            </figure>
+            <div className="card-body">
+                <h2 className=" text-sm md:text-md lg:text-xl">
+                    {productName} <span className="badge badge-xs badge-warning">{category}</span>
+                </h2>
+                <p className="text-sm text-gray-400">Price: {price}</p>
+                <p className="text-sm text-gray-400">Status: {status}</p>
+                <p className="text-sm text-gray-400">Rating: {rating}</p>
+            </div>
+        </Link >
+    );
+};
+
+export default FeaturedProduct;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import Banner from "../components/Ui/Banner";
+import FeaturedProduct from "../components/Ui/FeaturedProduct";
 import RootLayout from "../components/shared/Layouts/RootLayout";
 
 
-const HomePage = () => {
+const HomePage = ({ products }) => {
   return (
     <div>
       <Banner />
+      <div className="grid grid-cols-5 gap-4 px-4 mb-2 mt-6">
+        {
+          products?.data?.map((product) => <FeaturedProduct key={product?.id} product={product} />)
+        }
+      </div>
     </div>
   );
 };
@@ -16,4 +22,17 @@ export default HomePage;
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>
+}
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/api/v1/products");
+  const products = await res.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 30,
+  }
 }
