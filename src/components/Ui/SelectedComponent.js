965bf47268d07128ac6cfb9753c23e34038337e7
parvez13913 <parvezz13913@gmail.com>
2023-08-26 21:23:12 +0600

    modified pc builder page

--- src/components/Ui/SelectedComponent.js
+++ src/components/Ui/SelectedComponent.js
@@ -1,12 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 
 const SelectedComponent = ({ product }) => {
-    const { image, productName, price } = product
+    const { image, productName, price } = product;
     return (
         <div className='flex justify-between p-4 my-3 shadow-lg'>
             <div className='flex items-center space-x-8'>
                 <div>
-                    <img src={image} alt="product image" />
+                    <img
+                        src={image}
+                        alt="product image"
+                        style={{ aspectRatio: "300 / 200" }}
+                        className=" w-[80px] h-[50px]"
+                    />
                 </div>
                 <h1 className='font-serif'>{productName}</h1>
                 <h1 className='font-serif'>Price: {price}</h1>
